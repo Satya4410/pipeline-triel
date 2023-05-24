3d521f8132ca72110a1aa623bcb1e8ac716c71b0
Satya4410 <s.bharatsingh@dxc.com>
2023-05-24 14:49:41 +0530

    first commit

---
+++ src/main/java/com/example/dxc/PipelineTrielApplication.java
@@ -0,0 +1,13 @@
+package com.example.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineTrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineTrielApplication.class, args);
+	}
+
+}
